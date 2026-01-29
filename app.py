a6e067f66624b87e4ef3878f11b6b147b6199ed6
Artur Zieliński <artur.zielinski87@gmail.com>
2026-01-29 18:48:32 +0100

    init project

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def mypage():
+    return render_template("form.html")
+
+
+@app.route("/me", methods=['GET'])
+def me():
+    return render_template("me.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        text = request.form["message"]
+        print("Masz wiadomość:", text)
+    return render_template("contact.html")
+
+
